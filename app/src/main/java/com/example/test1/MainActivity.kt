d9e46b531ba207361f5a07dd599e083ae6f43046
DarkWister <darkwistercrak17@gmail.com>
2025-01-23 10:00:37 -0400

    Removed templates methods

--- app/src/main/java/com/example/test1/MainActivity.kt
+++ app/src/main/java/com/example/test1/MainActivity.kt
@@ -41,22 +41,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Test1Theme {
-        Greeting("Android")
-    }
-}
-
 @Preview(showBackground = true)
 @Composable
 fun notaView(){
